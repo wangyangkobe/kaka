d1a2737c81b4b0a11edc20bfa75aa8496f5b1ffb
wangyang <250801860@qq.com>
2016-07-15 22:45:06 +0800

    Implement api: /adimin/addMachines

--- kaka/admin/views.py
+++ kaka/admin/views.py
@@ -3,23 +3,49 @@ from flask import Blueprint, request, jsonify
 from kaka import models
 from kaka import db
 from kaka.decorators import verify_request_json, verify_request_token
+from webargs import fields
+from webargs.flaskparser import use_args
+from webargs.core import ValidationError
 
 admin_blueprint = Blueprint('admin', __name__)
 
+def checkUserNameExist(userName):
+    if models.User.getUserByUserName(userName):
+        return True
+    else:
+        raise ValidationError("The user \"{}\" don't exist!".format(userName))
+    
 @admin_blueprint.route('/addMachines', methods=['POST'])
 @verify_request_json
+@use_args({'User'     : fields.Str(required=True, validate=checkUserNameExist),
+           'Token'    : fields.Str(required=True),
+           'Machines' : fields.Nested({'mac'         : fields.Str(require=True),
+                                       'MachineName' : fields.Str(require=True)}, many=True),
+           'machineType' : fields.Int(missing=0)
+           },
+          locations = ('json',))
 @verify_request_token
-def addMachines():
-    userName = request.json.get("User", '')
-    machines = request.json.get("Machines", [])
+def addMachines(args):
+    userName = args.get("User", '')
+    machines = request.get_json().get("Machines", [])
+    print machines
+    user = models.User.getUserByUserName(userName)
     try:
         for machine in machines:
+            print machine
             macAddress  = machine.get('mac', '')
             machineName = machine.get("MachineName", '')
-            if not models.Machine.getMachineByMac(macAddress):
-                db.session.add(models.Machine(macAddress, machineName))
-            db.session.commit()
-            user = models.User.getUserByUserName(userName)
+            machineType = machine.get("MachineType", 0)
+            machineMoney= machine.get("MachineMoney", 0)
+            adminPass   = machine.get("adminPass", '')
+            userPass    = machine.get("UserPass", '')
+            machine = models.Machine(macAddress, machineName, machineType=machineType, 
+                                     machineMoney=machineMoney, adminPass=adminPass, userPass=userPass)
+            #db.session.add(machine)
+            user.machines.append(machine)
+        db.session.add(user)
+        db.session.commit()
+        
         return jsonify(request.json.get('Machines'))
     except Exception, error:
         return jsonify({'Status': 'Failed', 'StatusCode':-2, 'Msg': error.message}), 400
--- kaka/models.py
+++ kaka/models.py
@@ -3,18 +3,7 @@ from kaka import db
 import datetime
 from flask_login import UserMixin, make_secure_token
 from werkzeug.security import generate_password_hash, check_password_hash
-
-quanxian_table = db.Table('quanxian',
-    db.Column('userId', db.Integer, db.ForeignKey('user.id')),
-    db.Column('machineId', db.Integer, db.ForeignKey('machine.id')),
-    db.Column('permission', db.Integer),
-    db.Column('reason', db.String(200)),
-    db.Column('startTime', db.DateTime, default=datetime.datetime.utcnow),
-    db.Column('endTime', db.DateTime),
-    db.Column('money', db.Float),
-    db.Column('machineName', db.String(200)), #用户对机器的命名
-    db.PrimaryKeyConstraint('userId', 'machineId')
-    )
+from sqlalchemy.schema import PrimaryKeyConstraint
     
 class User(db.Model, UserMixin):
     __tablename__ = 'user'
@@ -30,6 +19,7 @@ class User(db.Model, UserMixin):
     regiserType = db.Column(db.Integer, unique=False)
     userMoney   = db.Column(db.Float)
     create_time = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    machines    = db.relationship('Machine',secondary='user_machine',backref=db.backref('user', lazy='dynamic'),lazy='dynamic')
     
     def __init__(self, username, password, phone = None, email = None, code = None, pushToken = None, userType = 0, registerType = 0, userMoney = 0.0):
         self.userName = username
@@ -73,16 +63,43 @@ class Machine(db.Model):
     id          = db.Column(db.Integer, primary_key=True, autoincrement=True)
     macAddress  = db.Column(db.String(100), nullable=False, unique=True)
     machineName = db.Column(db.String(100), nullable=False) #机器出厂时的设备名
-    machineType = db.Column(db.Boolean, nullable=False, default=False) #指定设备为“可匿名访问” 
+    machineType = db.Column(db.Integer, nullable=False, default=0) #指定设备为“可匿名访问” 
     machineMoney= db.Column(db.Float)
     adminPass   = db.Column(db.String(100)) #管理员密码
     userPass    = db.Column(db.String(100)) #用户密码
     
-    def __init__(self, macAddress, machineName):
-        self.macAddress = macAddress
-        self.machineName = machineName
+    #users = db.relationship("QuanXian", back_populates="user")
+    
+    def __init__(self, macAddress, machineName, machineType=0, machineMoney=0.0, adminPass=None, userPass=None):
+        self.macAddress   = macAddress
+        self.machineName  = machineName
+        self.machineType  = machineType
+        self.machineMoney = machineMoney
+        self.adminPass    = adminPass
+        self.userPass     = userPass
         
     @staticmethod
     def getMachineByMac(macAddress):
         return Machine.query.filter_by(macAddress=macAddress).first()
+    
+    def toJson(self):
+        return dict((c.name,
+                     getattr(self, c.name))
+                     for c in self.__table__.columns)
         
+class QuanXian(db.Model):
+    __tablename__  = 'user_machine'
+    __table_args__ = (PrimaryKeyConstraint('userId', 'machineId'),)
+    userId     = db.Column(db.Integer, db.ForeignKey('user.id'))
+    machineId  = db.Column(db.Integer, db.ForeignKey('machine.id'))
+    permission = db.Column(db.Integer)
+    reason     = db.Column(db.String(200))
+    startTime  = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    endTime    = db.Column(db.DateTime)
+    money      = db.Column(db.Float)
+    machineName= db.Column(db.String(200)) #用户对机器的命名
+ 
+    def toJson(self):
+        return dict((c.name,
+                     getattr(self, c.name))
+                     for c in self.__table__.columns)
