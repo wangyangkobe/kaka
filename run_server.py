32e75d20278233ce7a099ac8857af83277e84da5
wangyang <280801860@qq.com>
2016-09-27 13:48:36 +0800

    Update the Welcome message.

--- run_server.py
+++ run_server.py
@@ -4,7 +4,10 @@ from flask import request
 
 @app.route('/')
 def index():
-    return 'Welocome to site: {}'.format(request.url_root)
+    if request.url_root.find("5000") != -1:
+        return '亲爱的，欢迎来到正式版!'
+    else:
+        return '亲爱的，欢迎来到开发版!'
       
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded=False)
+    app.run(host='0.0.0.0', port=5001, debug=False, threaded=False)
