df15146f2cfba8395f37c073ad251b3793247a7c
wangyang <280801860@qq.com>
2016-10-11 16:50:31 +0800

    Use gunicorn to deploy the project.

--- run_server.py
+++ run_server.py
@@ -10,4 +10,4 @@ def index():
         return '亲爱的，欢迎来到开发版!'
       
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=False, threaded=False)
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
