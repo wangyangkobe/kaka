a4fc1d05bbc6208fad4d65ebde2007a9d4258167
wangyang <280801860@qq.com>
2016-10-12 15:12:37 +0800

    add manage.py for database Migration.

---
+++ manage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+from kaka import app, db
+
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == '__main__':
+    manager.run()
